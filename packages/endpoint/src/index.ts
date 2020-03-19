6b927e2159a22609b740711be2a113821858121b
jeanfortheweb <jeanfortheweb@gmail.com>
2020-03-19 22:30:10 +0100

    feat(endpoint): add template feature

--- packages/endpoint/src/index.ts
+++ packages/endpoint/src/index.ts
@@ -4,6 +4,23 @@ import { query as _query, Query } from '@apitizer/query';
 export type MaybeCallback<T> = T | Callback<T>;
 export type Callback<T> = (value: T) => T;
 
+export interface Template<Args extends any[], Resource = any, Data = Resource> {
+  (...args: Args): Endpoint<Resource, Data>;
+}
+
+export interface Factory<
+  Args extends any[],
+  SourceResource,
+  SourceData,
+  TemplateResource,
+  TemplateData
+> {
+  (endpoint: Endpoint<SourceResource, SourceData>, ...args: Args): Endpoint<
+    TemplateResource,
+    TemplateData
+  >;
+}
+
 /**
  * Defines an endpoint builder.
  */
@@ -22,7 +39,7 @@ export interface Endpoint<Resource = any, Data = Resource> {
    */
   many<InnerResource = Resource, InnerData = Data>(
     path: string
-  ): Endpoint<InnerResource, InnerData>;
+  ): Endpoint<InnerResource[], InnerData>;
 
   /**
    * Creates a deeper nested endpoint based on the current endpoint,
@@ -47,6 +64,19 @@ export interface Endpoint<Resource = any, Data = Resource> {
    * @param query Query or callback
    */
   query(query: MaybeCallback<Query>): Endpoint<Resource, Data>;
+
+  /**
+   * Creates a template function to create a deeper endpoint based on certain parameters.
+   *
+   * @param factory Factory.
+   */
+  template<
+    TemplateResource = Resource,
+    TemplateData = Data,
+    Args extends any[] = any[]
+  >(
+    factory: Factory<Args, Resource, Data, TemplateResource, TemplateData>
+  ): Template<Args, TemplateResource, TemplateData>;
 }
 
 /**
@@ -64,12 +94,14 @@ export function endpoint<Resource = any, Data = Resource>(
     get: (urlOnly = false) =>
       urlOnly || query.empty() ? url : [url, query.get()].join('?'),
 
-    many: (path: string) => endpoint([url, path].join('/'), query),
+    many: path => endpoint([url, path].join('/'), query),
 
-    one: (path: string, id: number | string) =>
-      endpoint([url, path, id].join('/'), query),
+    one: (path, id) => endpoint([url, path, id].join('/'), query),
 
-    query: (value: MaybeCallback<Query>) =>
-      endpoint(url, typeof value === 'function' ? value(query) : value)
+    query: value =>
+      endpoint(url, typeof value === 'function' ? value(query) : value),
+
+    template: factory => (...args: any[]): any =>
+      factory(endpoint(url, query), ...(args as any))
   });
 }
