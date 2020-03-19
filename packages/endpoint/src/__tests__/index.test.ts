a5df26e1f6bf04d421875fbce76483b6906ae4fc
jeanfortheweb <jeanfortheweb@gmail.com>
2020-03-19 22:30:10 +0100

    feat(endpoint): add template feature

--- packages/endpoint/src/__tests__/index.test.ts
+++ packages/endpoint/src/__tests__/index.test.ts
@@ -57,3 +57,11 @@ describe('query', () => {
     );
   });
 });
+
+it('should produce the expected template', () => {
+  const factory = jest.fn((endpoint, a, b) => endpoint.one('a', a).one('b', b));
+  const template = endpoint('http://foo.bar').template(factory);
+
+  expect(template(1, 2).get()).toEqual('http://foo.bar/a/1/b/2');
+  expect(factory).toHaveBeenCalledTimes(1);
+});
